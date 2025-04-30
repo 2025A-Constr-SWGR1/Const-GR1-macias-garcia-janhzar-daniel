da44a3ed634056dae69205ec5e96db2fb44362e0
janhzarmacias <janhzar.macias@epn.edu.ec>
2025-04-30 15:34:16 -0500

    a

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { error } from 'console';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    console.log('log!');
+    console.debug('debug!');
+    console.info('info!');
+    console.warn('warn!');
+    console.error('error!');
+    return 'Hello World!!';
   }
 }
